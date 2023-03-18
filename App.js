4c05da6e4ce8b803894d003def11bf049da3719d
leen_odeh <123558998+Leen-odeh12@users.noreply.github.com>
2023-03-18 14:12:42 +0200

    Create App.js

---
+++ App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./components/Header/header";
+import Footer from "./components/Footer/footer";
+import Body from "./components/Body/body";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
